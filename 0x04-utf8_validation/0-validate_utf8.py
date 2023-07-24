0839baabde8bde025f819827e4a9c9f4ed451101
bollo-omar <bolloaggrey@gmail.com>
2023-07-24 22:16:59 +0000

    Refactored code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
+""" UTF-8 Validation """
+
 def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+    """
     num_bytes = 0
 
     for byte in data:
